d274e9762404e7e200996bbd4ed0f1d43888ad19
vsareen0 <vsareen24@gmail.com>
2020-03-18 17:07:59 +0530

    updated

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req,res) => {
-    res.render('index.html');
+    fs.readFile('index.html',(err,data)=>{
+        if (err) console.log(err);
+        res.render(data);   
+    });
 });
-
-app.use('/static',express.static(__dirname,'/public'));
 
 app.listen(PORT, () => console.log(`The server is listening on port ${PORT}`));
