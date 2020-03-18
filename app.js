3d7c23a5b09ab0c1eb546844acd1502ce51951f0
vsareen0 <vsareen24@gmail.com>
2020-03-18 17:15:02 +0530

    Working

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
 const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
@@ -7,8 +8,10 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req,res) => {
     fs.readFile('index.html',(err,data)=>{
         if (err) console.log(err);
-        res.render(data);   
+        res.send(data.toString());   
     });
 });
+
+app.use('/static',express.static(path.join(__dirname+'/public')));
 
 app.listen(PORT, () => console.log(`The server is listening on port ${PORT}`));
